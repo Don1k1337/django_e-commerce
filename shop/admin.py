9fd55c316c39e063acd4ad1b1181451c3b729d90
Don1k1337 <dan4ik1411@gmail.com>
2020-08-19 19:21:58 +0600

    Modified models.py added get_absolute_url method, implemented product_list, product_detail view, created simple detail and list templates for displaying goods.

--- shop/admin.py
+++ shop/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from .models import Category, Product
 
 
+# prepopulated_fields used to specify fields where
+# the value is automatically set using the value of other fields
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price',
+                    'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    # list_editable can be used for editing multiply rows at once
+    # it used only after list_display attr
+    list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
